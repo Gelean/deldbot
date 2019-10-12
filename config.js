8c41c0e30f6e7e9e6996d7a50f0c82da9b552fbe
Derek Elder <Gelean@users.noreply.github.com>
2019-10-12 10:14:23 -0700

    Add new keys

--- config.js
+++ config.js
@@ -1,13 +1,15 @@
 module.exports = {
-  'clientId'     : 'CLIENT_ID',
-  'clientSecret' : 'CLIENT_SECRET',
-  'botUsername'  : 'BOT_NAME',
-  'botToken'     : 'BOT_TOKEN',
-  'hostname'     : 'PLEX_IP',
-  'port'         : 'PLEX_PORT',
-  'plexId'       : 'PLEX_ID',
-  'plexUsername' : 'PLEX_USERNAME',
-  'plexPassword' : 'PLEX_PASSWORD',
-  'plexToken'    : 'PLEX_TOKEN',
-  'omdbKey'      : 'OMDB_KEY'
+  'clientId'          : 'CLIENT_ID',
+  'clientSecret'      : 'CLIENT_SECRET',
+  'botUsername'       : 'BOT_NAME',
+  'botToken'          : 'BOT_TOKEN',
+  'hostname'          : 'PLEX_IP',
+  'port'              : 'PLEX_PORT',
+  'plexId'            : 'PLEX_ID',
+  'plexUsername'      : 'PLEX_USERNAME',
+  'plexPassword'      : 'PLEX_PASSWORD',
+  'plexToken'         : 'PLEX_TOKEN',
+  'omdbKey'           : 'OMDB_KEY',
+  'imgurClientId'     : 'IMGUR_CLIENT_ID',
+  'imgurClientSecret' : 'IMGUR_CLIENT_SECRET'
 };
