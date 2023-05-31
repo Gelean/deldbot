78c5ca839b731dc8aa05d0da3c25714cc6d0222a
Derek Elder <Gelean@users.noreply.github.com>
2023-05-31 19:14:05 -0400

    Fix crypto command

--- src/commands/crypto.js
+++ src/commands/crypto.js
@@ -1,9 +1,5 @@
-// This command is now broken:
-// https://www.quantconnect.com/forum/discussion/14508/binance-support-just-said-that-they-are-no-longer-working-with-us-based-ips/p1
-
 const config = require('../../.env/config.json')
 const https = require('https')
-const { EmbedBuilder } = require('discord.js')
 
 module.exports = {
   name: 'crypto',
@@ -14,52 +10,36 @@ module.exports = {
   cooldown: 1,
   aliases: ['cryptocurrency', 'cryptocurrencies'],
   execute (message, args) {
+    if (config.coinmarketcap.key === 'COINMARKETCAP_KEY') {
+      return message.channel.send('The coinmarketcap.key is blank, set it in config.json')
+    }
+
     let cryptocurrencies = args
     // console.log(`Query: ${cryptocurrencies}`)
 
-    for(let i = 0; i < cryptocurrencies.length; i++) {
-      let cryptoQuery = `https://api.binance.com/api/v3/ticker/price?symbol=${cryptocurrencies[i].toUpperCase()}USDT`
+    for (let i = 0; i < cryptocurrencies.length; i++) {
+      let url = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?CMC_PRO_API_KEY=${config.coinmarketcap.key}&symbol=${cryptocurrencies[i]}`
 
-      https.get(cryptoQuery, (resp) => {
+      https.get(url, (resp) => {
         let data = cryptoData = ''
-  
+
         resp.on('data', (chunk) => {
           data += chunk
         })
-  
+
         resp.on('end', () => {
           cryptoData = JSON.parse(data)
-          // console.log(`data: ${data}`)
-
-          if (cryptoData.code === null) {
-            const currentDate = new Date()
-            let month = (1 + currentDate.getMonth()).toString()
-            month = month.length > 1 ? month : '0' + month
-            let day = currentDate.getDate().toString()
-            day = day.length > 1 ? day : '0' + day
-            let year = currentDate.getFullYear()
-            cryptoDate = `${year}-${month}-${day}`
-
-            let cryptoEmbed = new EmbedBuilder()
-              .setColor('F0B90B')
-              .setURL(`https://www.binance.com/en/trade/${cryptocurrencies[i].toUpperCase()}_USDT`)
-              .setTitle(cryptocurrencies[i].toUpperCase())
-              .setFooter({text: 'Binance', iconURL: 'https://i.imgur.com/4Rm5H8z.png'})
-
-            cryptoEmbed.addField('Date', `${cryptoDate}`, true)
-            if (cryptoData.price >= 100) {
-              cryptoEmbed.addField({name: 'Price', value: `$${Math.round(cryptoData.price)}`, inline: true})
-            } else {
-              cryptoEmbed.addField({name: 'Price', value: `$${cryptoData.price}`, inline: true})
-            }
-            
-            message.channel.send({ embeds: [cryptoEmbed] });
+          // console.log(cryptoData)
+          // console.log(cryptoData["data"])
+          if (cryptoData === '' || cryptoData === null || cryptoData === undefined || cryptoData["data"][cryptocurrencies[i]] === undefined) {
+            message.channel.send('No results found, please refine your search dimwit')
           } else {
-            message.channel.send(`No cryptocurrency found for ${cryptocurrencies[i].toUpperCase()} symbol dimwit`)
+            message.channel.send(`The price of ${cryptocurrencies[i]} is: $${cryptoData["data"][cryptocurrencies[i]]["quote"]["USD"]["price"].toFixed(2)}`)
           }
         })
       }).on('error', (err) => {
-        message.channel.send(`An error has occurred, go yell at <@${config.owner.id}>`)
+        console.error(err)
+        message.channel.send(`An error has occurred: ${err}`)
       })
     }
   }
